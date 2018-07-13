dde98293453fa92c6114adaac38d6b8372d654c3
Halo Guan <doresimon@163.com>
2018-07-13 16:44:28 +0800

    update

--- api/static/dashboard.js
+++ api/static/dashboard.js
@@ -1,6 +1,7 @@
 
     new Vue({
-      el: '#cheshire-dashboard',
+      el: '#main',
+      // el: '#cheshire-dashboard',
       data: {
         colorMap: {
           babyblue: '#4eb4f9',
@@ -21,8 +22,24 @@
         contracts: {},
         kitties: [],
         users: [],
+        import_data:{
+          mainID: 0,
+          newID:0,
+          user:{},
+          busy:0,
+        },
+        create_data:{
+          matronId:0,
+          sireId:0,
+          generation:0,
+          genes:0,
+          owner:{},
+          apiObject:0,
+          busy:0,
+        },
       },
 
+      // async created () {
       async mounted () {
         this.contracts = (await axios.get('/cheshire/contracts')).data
         this.kitties = (await axios.get('/kitties?limit=-1')).data.kitties.map((kitty) => {
@@ -31,4 +48,54 @@
         })
         this.users = (await axios.get('/cheshire/users')).data.users
       },
+
+      // new methods
+      methods:{
+
+        //import a cat by ID and user.
+        importCat: async function(ID,address){
+          var _this = this
+
+          _this.import_data.busy = 1
+
+          const params = {
+            kittyIdMainnet:ID,
+            ownerTestnet:address,
+          }
+
+          this.import_data.newID = (await axios.post('/cheshire/kitties/import',params)).data.kittyId
+
+          var d = (await axios.get('/kitties/'+this.import_data.newID)).data
+
+          this.kitties.push(d)
+
+          _this.import_data.busy = 0
+        },
+
+        // create cat
+        // createKitty(matronId, sireId, generation, genes, owner, apiObject)
+        createCat: async function(matronId, sireId, generation, genes, owner, apiObject){
+          var _this = this
+
+          _this.create_data.busy = 1
+
+          const params = {
+            matronId:matronId,
+            sireId:sireId,
+            generation:generation,
+            genes:genes,
+            owner:owner,
+            apiObject:apiObject,
+          }
+
+          this.create_data.newID = (await axios.post('/cheshire/kitties',params)).data.kittyId
+
+          var d = (await axios.get('/kitties/'+this.create_data.newID)).data
+
+          this.kitties.push(d)
+
+          _this.create_data.busy = 0
+        },
+
+      }
     })
---
+++ ins.md
@@ -0,0 +1,19 @@
+# cheshire
+
+## Usage
+
+install `yarn`
+
+install `ganache-cli`
+
+> git clone https://github.com/Doresimon/cheshire
+
+> cd cheshire
+
+> yarn install
+
+> yarn start
+
+## tips
+
+- windows: change `truffle.js` to `truffle-config.js`
